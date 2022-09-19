302c902bf3cff5f0cf2da9b7f3b6aa8c31006da7
César Vargas <cesar@vargas.dev>
2022-09-18 21:47:27 -0300

    add prettierrc

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,41 @@
-// const escpos = require('escpos');
+// const escpos = require('escpos')
 import escpos, { USB } from 'escpos'
 // install escpos-usb adapter module manually
-escpos.USB = require('escpos-usb');
+escpos.USB = require('escpos-usb')
 // Select the adapter based on your printer type
-const printerList : any = escpos.USB.findPrinter()
+const printerList: any = escpos.USB.findPrinter()
 const { idVendor, idProduct } = printerList[0].deviceDescriptor
-const device  = new escpos.USB(idVendor, idProduct);
+const device = new escpos.USB(idVendor, idProduct)
 
-// const device  = new escpos.Network('localhost');
-// const device  = new escpos.Serial('/dev/usb/lp0');
+// const device  = new escpos.Network('localhost')
+// const device  = new escpos.Serial('/dev/usb/lp0')
 
-const options = { encoding: "UTF-8" /* default */ }
+const options = { encoding: 'UTF-8' /* default */ }
 // encoding is optional
 
-const printer = new escpos.Printer(device, options);
+const printer = new escpos.Printer(device, options)
 
 function print(title: string, author: string, url: string, created_at: string) {
   device.open((error: any) => {
     printer
-    .font('B')
-    .size(2, 2)
-    .align('CT')
-    .style('BU')
-    .text('PR Review')
-    .size(1, 1)
-    .align('LT')
-    .style('NORMAL')
-    .text('')
-    .text(`url: ${url}`)
-    .text(`author: ${author}`)
-    .text(`created_at: ${created_at}`)
-    .text('')
-    .text(`title: ${title}`)
-    .text('')
-    .text('')
-    .cut().close()
+      .font('B')
+      .size(2, 2)
+      .align('CT')
+      .style('BU')
+      .text('PR Review')
+      .size(1, 1)
+      .align('LT')
+      .style('NORMAL')
+      .text('')
+      .text(`url: ${url}`)
+      .text(`author: ${author}`)
+      .text(`created_at: ${created_at}`)
+      .text('')
+      .text(`title: ${title}`)
+      .text('')
+      .text('')
+      .cut()
+      .close()
   })
 }
 
@@ -42,29 +43,35 @@ import express, { Express, Request, Response } from 'express'
 import dotenv from 'dotenv'
 import bodyParser from 'body-parser'
 
-dotenv.config();
+dotenv.config()
 
-const app: Express = express();
+const app: Express = express()
 app.use(bodyParser.json())
 
-const port = process.env.PORT || 3333;
+const port = process.env.PORT || 3333
 
 app.post('/github', (req: Request, res: Response) => {
   const { action } = req.body
 
-  switch(action) {
+  switch (action) {
     case 'review_requested':
-      const { pull_request: { title, url, user: { login: author }, created_at }, requested_reviewer: { login: reviewer } } = req.body
+      const {
+        pull_request: {
+          title,
+          url,
+          user: { login: author },
+          created_at,
+        },
+        requested_reviewer: { login: reviewer },
+      } = req.body
       if (reviewer === 'cesarvargas00') {
         console.log(title)
         print(title, author, url, created_at)
       }
   }
-  res.send('ok');
-});
+  res.send('ok')
+})
 
 app.listen(port, () => {
-  console.log(`[server]: Server is running at https://localhost:${port}`);
-});
-
-
+  console.log(`[server]: Server is running at https://localhost:${port}`)
+})
